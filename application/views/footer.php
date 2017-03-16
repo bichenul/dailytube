5d563b589011d000e80e84f076801364567d8b64
bichenul <nicolas@lenec.fr>
2017-03-16 16:51:48 +0100

    modification footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
-    <footer class="container-fluid text-center">
-      <p>Footer Text</p>
-    </footer>
+		<footer class="container-fluid text-center">
+		  <p>DailyTube</p>
+		</footer>			
     <script type="text/javascript" src="<?php echo base_url ();?>assets/jquery/external/jquery/jquery.js"></script>
     <script type="text/javascript" src="<?php echo base_url ();?>assets/javascript/dailytube.js"></script>
     <script type="text/javascript" src="<?php echo base_url ();?>assets/bootstrap/js/bootstrap.min.js"></script>
